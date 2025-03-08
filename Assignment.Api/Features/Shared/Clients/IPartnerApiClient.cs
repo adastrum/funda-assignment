564915a82957ad0318957c9e562ed513210bc2a5
Dmitriy Trofimov <trofimovde@outlook.com>
2025-03-08 11:48:12 +0100

    Add partner API client

---
+++ Assignment.Api/Features/Shared/Clients/IPartnerApiClient.cs
@@ -0,0 +1,23 @@
+using Refit;
+
+namespace Assignment.Api.Features.Shared.Clients;
+
+public interface IPartnerApiClient
+{
+    [Get("/feeds/Aanbod.svc/json/{apiKey}")]
+    Task<ApiResponse<GetObjectsForSaleResponse>> GetObjectsForSale(string apiKey, [Query] GetObjectsForSaleQueryParams queryParams);
+}
+
+public record GetObjectsForSaleQueryParams
+{
+    [AliasAs("type")] public string Type { get; init; } = "koop";
+    [AliasAs("zo")] public string Query { get; init; }
+    [AliasAs("page")] public int Page { get; init; }
+    [AliasAs("pagesize")] public int PageSize { get; init; }
+}
+
+public record GetObjectsForSaleResponse(ObjectForSale[] Objects, Paging Paging, int TotaalAantalObjecten);
+
+public record ObjectForSale(string Id, int MakelaarId, string MakelaarNaam);
+
+public record Paging(int AantalPaginas, int HuidigePagina);
