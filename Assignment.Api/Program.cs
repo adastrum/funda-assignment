b657b68b78a607a401b1eace991c64ca01710464
Dmitriy Trofimov <trofimovde@outlook.com>
2025-03-08 11:23:06 +0100

    Add health check

--- Assignment.Api/Program.cs
+++ Assignment.Api/Program.cs
@@ -1,8 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
-
 app.MapGet("/", () => "Hello World!");
-
+app.MapHealthChecks("/api/health");
 app.Run();
 
 public partial class Program
