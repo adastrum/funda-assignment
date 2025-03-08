dc791284358b9055c4ea20cddd6d4a2c220cb60a
Dmitriy Trofimov <trofimovde@outlook.com>
2025-03-08 13:53:20 +0100

    Use polly to retry calls to partner API

--- Assignment.Api/Features/Shared/ServiceCollectionExtensions.cs
+++ Assignment.Api/Features/Shared/ServiceCollectionExtensions.cs
@@ -1,4 +1,7 @@
+using System.Net;
 using Assignment.Api.Features.Shared.Clients;
+using Polly;
+using Polly.Extensions.Http;
 using Refit;
 
 namespace Assignment.Api.Features.Shared;
@@ -9,10 +12,24 @@ public static class ServiceCollectionExtensions
         ConfigurationManager configuration)
     {
         services.AddRefitClient<IPartnerApiClient>()
-            .ConfigureHttpClient(httpClient => httpClient.BaseAddress = new Uri(configuration["PartnerApi:BaseUrl"]));
+            .ConfigureHttpClient(httpClient => httpClient.BaseAddress = new Uri(configuration["PartnerApi:BaseUrl"]))
+            .AddPolicyHandler(GetRetryTransientErrorsPolicy());
 
         services.Configure<PartnerApiOptions>(configuration.GetSection("PartnerApi"));
 
         return services;
+    }
+
+    private static IAsyncPolicy<HttpResponseMessage> GetRetryTransientErrorsPolicy()
+    {
+        const int retryCount = 5;
+        const int baseDelayInSeconds = 2;
+        
+        return HttpPolicyExtensions
+            .HandleTransientHttpError()
+            // while it should be HTTP 429 Too Many Requests, it's not implemented in the Partner API
+            .OrResult(response => response.StatusCode == HttpStatusCode.Unauthorized)
+            .WaitAndRetryAsync(retryCount, retryAttempt =>
+                TimeSpan.FromSeconds(Math.Pow(baseDelayInSeconds, retryAttempt)));
     }
 }
