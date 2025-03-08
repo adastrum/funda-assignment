209a5879e58c2bb7c53f9eb779cc47c51fd2ec8b
Dmitriy Trofimov <trofimovde@outlook.com>
2025-03-08 11:44:19 +0100

    Support OpenApi with Scalar

--- Assignment.Api/Program.cs
+++ Assignment.Api/Program.cs
@@ -1,8 +1,15 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHealthChecks();
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
 app.MapHealthChecks("/api/health");
 app.Run();
 
