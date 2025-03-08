9a18cfe9799b1afd6c48727d173d0336ca547b95
Dmitriy Trofimov <trofimovde@outlook.com>
2025-03-08 11:22:01 +0100

    Test health check

---
+++ Assignment.Api.Tests/ApiTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Assignment.Api.Tests;
+
+public class ApiTests : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly HttpClient _client;
+
+    public ApiTests(CustomWebApplicationFactory factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Health_check()
+    {
+        var response = await _client.GetAsync("/api/health");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+}
